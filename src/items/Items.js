02d2b1a1a672d7620ab13052da063a3c47272691
Batuhan TANIR <batuhan.tanir00@gmail.com>
2023-10-13 22:24:34 +0300

    project completed

--- src/items/Items.js
+++ src/items/Items.js
@@ -25,7 +25,102 @@ const Items = [
     checked: false,
     show: false,
   },
-  
+  {
+    id: nanoid(),
+    name: "html",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "html",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "javascript",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "javascript",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "nodejs",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "nodejs",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "python",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "python",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "redux",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "redux",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "router",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "router",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "tailwind",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "tailwind",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "vscode",
+    checked: false,
+    show: false,
+  },
+  {
+    id: nanoid(),
+    name: "vscode",
+    checked: false,
+    show: false,
+  },
 ];
 
 export default Items;
